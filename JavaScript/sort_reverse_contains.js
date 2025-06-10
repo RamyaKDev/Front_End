c45beb61336018f89dc5e152b2fc0ef9788f123f
Ramya <ramyakpass@gmail.com>
2025-06-09 22:21:44 -0400

    sort reverse contains in javascript

---
+++ JavaScript/sort_reverse_contains.js
@@ -0,0 +1,13 @@
+//sort() - sorts element based on ASCII value
+let sorta=[20,12,34,22,6];
+console.log(sorta.sort());
+//output: [ 12, 20, 22, 34, 6 ]
+
+//reverse()
+let ta=[20,12,34,22,6];
+console.log(ta.reverse());
+
+
+//includes()-returns boolean value. Checks whether the given element is available or not
+let a=[20,12,34,22,6];
+console.log(a.includes(121));   
